a69a4db703296fb08f8257c1c53d0eb53867ae3f
Muhammad Waqar <65023745+MuhammadWaqar3325@users.noreply.github.com>
2022-04-19 03:35:43 +0500

    use-state
    Use state sy upper casse or lower case ma convert kia

--- my-word-app/src/Components/TextForm/Text.js
+++ my-word-app/src/Components/TextForm/Text.js
@@ -2,19 +2,42 @@ import React,{ useState } from 'react'
 
 
 export default function Text(props) {
-  const [Text, setText] = useState('Enter Text ')
+
+  
+ 
+  const [Text, setText] = useState('Enter Text    Here');
+  const HandleOnChange = (textarea)=>{
+    // console.log("change")
+    setText(textarea.target.value)
+  }
+
+  const HandleUp = ()=>{
+    // console.log("uper")
+    let newText = Text.toLocaleUpperCase();
+    setText(newText);
+  }
+  const HandleLow = ()=>{
+    // console.log("low")
+    let newText = Text.toLowerCase();
+    setText(newText);
+  }
+
 
   return (
-
+<>
   <div>
     <h1>{props.heading}</h1>
     <div className="mb-3 my-3" >
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="14"></textarea>
+    <textarea className="form-control" id="exampleFormControlTextarea1" onChange={HandleOnChange}  value={Text} itemID="mybox" rows="14"></textarea>
     </div>
-      <button className="btn btn-primary m-2">Change To Upper</button>
-      <button className="btn btn-primary m-2">Change To Lower</button>
+      <button className="btn btn-primary m-2" onClick={HandleUp}>Change To Upper</button>
+      <button className="btn btn-primary m-2"onClick={HandleLow}>Change To Lower</button>
 
   </div>
-   
+  <div className="container">
+    <h1>Your Text Summary</h1>
+    <p>{Text.split}</p>
+  </div>
+  </>
   )
 }
