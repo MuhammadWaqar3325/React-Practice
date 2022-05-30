5435614859b384b8de3beb36a442380e7ddcf237
Muhammad Waqar <65023745+MuhammadWaqar3325@users.noreply.github.com>
2022-05-30 23:58:29 +0500

    hami

--- New folder/New folder (2)/hami-web/src/Components/pages/Protect.js
+++ New folder/New folder (2)/hami-web/src/Components/pages/Protect.js
@@ -9,13 +9,13 @@ function Protect() {
         <div >
             <div className='container' style={{}}>
                 <div>
-                    <h5 style={{ color: "red", fontFamily: "-moz-initial" }}>
+                    <h5 style={{ color: "red", fontFamily: "-moz-initial",fontStyle:"oblique" }}>
                         ______WHAT WE DO
                     </h5>
                 </div>
 
                 <div>
-                    <h3 style={{ color: "blueviolet", fontFamily: "sans-serif", fontWeight: "bold", }}>
+                    <h3 style={{ color:"#393D72", fontFamily: "sans-serif", fontWeight: "bold",fontStyle:"italic" }}>
                         WHAT WE DO TO PROTECT ANIMALS
                     </h3>
                 </div>
@@ -24,6 +24,8 @@ function Protect() {
                     <div className='card' style={{ width: "17rem",border:"none" }}>
                         <img src={LoveDog} style={{borderRadius:"10%"}} className="card-img-top" alt="..." />
                         <div className="card-body">
+                        <h5 style={{fontSize:"18px",fontStyle:"italic",fontWeight:"bold",color:"#393D72"}}>
+                            Raising money to help</h5>
                             <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
@@ -31,12 +33,17 @@ function Protect() {
                     <div className='card' style={{ width: "17rem",border:"none" }}>
                         <img src={DogOutdoor} style={{borderRadius:"10%"}} className="card-img-top" alt="..." />
                         <div className="card-body">
+                        <h5 style={{fontSize:"18px",fontStyle:"italic",fontWeight:"bold",color:"#393D72"}}>
+                            Сlose work with services
+                            </h5>
                             <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                     <div className='card' style={{ width: "17rem",border:"none" }}>
                         <img src={LittleBoy} style={{borderRadius:"10%"}} className="card-img-top" alt="..." />
                         <div className="card-body">
+                        <h5 style={{fontSize:"18px",fontStyle:"italic",fontWeight:"bold",color:"#393D72"}}>
+                            Pro Guided tours only</h5>
                             <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
@@ -44,6 +51,8 @@ function Protect() {
                     <div className='card' style={{ width: "17rem",border:"none" }}>
                         <img src={Twomixed} style={{borderRadius:"10%"}} className="card-img-top" alt="..." />
                         <div className="card-body">
+                        <h5 style={{fontSize:"18px",fontStyle:"italic",fontWeight:"bold",color:"#393D72"}}>
+                            Protecting animal area</h5>
                             <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
