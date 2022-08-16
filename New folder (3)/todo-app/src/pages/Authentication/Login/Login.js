161312d1c00e5d268e01cab0e7954b1b9471ac65
Muhammad Waqar <65023745+MuhammadWaqar3325@users.noreply.github.com>
2022-08-16 10:05:31 +0500

    ss

--- New folder (3)/todo-app/src/pages/Authentication/Login/Login.js
+++ New folder (3)/todo-app/src/pages/Authentication/Login/Login.js
@@ -1,26 +1,41 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from 'config/firebase'
 
 
 
-
-const initialState =  {email : "" , password: ""}
+const initialState = { email: "", password: "" }
 
 
 function Login() {
 
-const [state, setState] = useState(initialState)
+    const [state, setState] = useState(initialState)
+    const [isProcessing, setIsProcessing] = useState(false)
 
-const handleChange  = e=>{
-    setState(s =>({...s,[e.target.name]:e.target.value}))
-}
+    const handleChange = e => {
+        setState(s => ({ ...s, [e.target.name]: e.target.value }))
+    }
 
-const handleLogin = e => {
-    e.preventDefault()
-    let { email, password } = state
+    const handleLogin = e => {
+        e.preventDefault()
+        let { email, password } = state
 
-    console.log(email)
-    console.log(password)
-}
+        setIsProcessing(true)
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                let user = userCredential.user
+                console.log(user)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            .finally(() => {
+                setIsProcessing(false)
+            })
+
+
+    }
 
 
     return (
@@ -28,23 +43,41 @@ const handleLogin = e => {
         <div className='auth'>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3'>
-                        <div className='card py-2 py-md-3 py-lg-4'>
-                            <h1>
+                    <div className='col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3'>
+                        <div className='card p-2 p-md-3 p-lg-4'>
+                            <h3 className='mb-4'>
                                 LOGIN
-                            </h1>
-                            <div className='row'>
-                                <div className='col'>
-                                    <label htmlFor='email'> Email</label>
-                                    <input type="email" className='form-control' placeholder='Email' name='email'onChange={handleChange}  />
+                            </h3>
+                            <form onSubmit={handleLogin}>
+                                <div className='row mb-3'>
+                                    <div className='col'>
+                                        <label htmlFor='email'> Email</label>
+                                        <input type="email" className='form-control' placeholder='Email' name='email' onChange={handleChange} />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='row'>
-                                <div className='col'>
-                                    <label htmlFor='password'> password</label>
-                                    <input type="password" className='form-control' placeholder='password' name='password'onChange={handleChange}  />
+                                <div className='row mb-3'>
+                                    <div className='col'>
+                                        <label htmlFor='password'> password</label>
+                                        <input type="password" className='form-control' placeholder='password' name='password' onChange={handleChange} />
+                                    </div>
                                 </div>
-                            </div>
+                                <div className='row'>
+                                    <div className='col'>
+                                        <button className='btn w-100 ' disabled={isProcessing}>
+                                            {!isProcessing
+                                                ? "Login"
+                                                : <div className='spinner-grow    spinner-grow-sm   '></div>
+                                            }
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className='row mt-4'>
+                                    <div className='col'>
+                                        <p className='mt-0 text-center'>Need an Account? <Link to="/authentication/Register" className='text-dark'>Register</Link> </p>
+                                    </div>
+                                </div>
+                            </form>
+
                         </div>
                     </div>
                 </div>
--- New folder (3)/todo-app/src/pages/Authentication/Register/Register.js
+++ New folder (3)/todo-app/src/pages/Authentication/Register/Register.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from 'config/firebase'
+const initialState = { email: "", password: "" }
+
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
+
+    const [state, setState] = useState(initialState)
+    const [isProcessing, setIsProcessing] = useState(false)
+
+    const handleChange = e => {
+        setState(s => ({ ...s, [e.target.name]: e.target.value }))
+    }
+
+    const handleRegister = e => {
+        e.preventDefault()
+        let { email, password } = state
+
+        setIsProcessing(true)
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                let user = userCredential.user
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            .finally(() => {
+                setIsProcessing(false)
+            })
+       
+    }
+
+
+    return (
+
+        <div className='auth'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3'>
+                        <div className='card p-2 p-md-3 p-lg-4'>
+                            <h3 className='mb-4'>
+                                REGISTER
+                            </h3>
+                            <form onSubmit={handleRegister}>
+                                <div className='row mb-3'>
+                                    <div className='col'>
+                                        <label htmlFor='email'> Email</label>
+                                        <input type="email" className='form-control' placeholder='Email' name='email' onChange={handleChange} />
+                                    </div>
+                                </div>
+                                <div className='row mb-3'>
+                                    <div className='col'>
+                                        <label htmlFor='password'> password</label>
+                                        <input type="password" className='form-control' placeholder='password' name='password' onChange={handleChange} />
+                                    </div>
+                                </div>
+                                <div className='row'>
+                                    <div className='col'>
+                                        <button className='btn w-100 ' disabled={isProcessing}>
+                                            {!isProcessing
+                                                ? "Login"
+                                                : <div className='spinner-grow    spinner-grow-sm   '></div>
+                                            }
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className='row mt-4'>
+                                    <div className='col'>
+                                        <p className='mt-0 text-center'>Already have an Account? <Link to="/authentication/login" className='text-dark'>Login</Link> </p>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default Register
